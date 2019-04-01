bda57d51c4bfbb257c2739a8da30c14118b8d61e
Matthew Walls <matthew.walls@guardian.co.uk>
2019-04-01 16:01:12 +0100

    Stub in remote SlotsAPI and hello world page.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import SlotsAPI from './proxies/slotsAPI'
 
 class App extends Component {
+
+  constructor() {
+
+    super();
+
+    this.slotsAPI = new SlotsAPI();
+
+    this.state = {
+      slotComponents: [],
+    };
+
+  }
+
+  componentDidMount() {
+
+    // contact 3rd party services, etc
+
+    this.setState({
+      slotComponents: this.slotsAPI.all(),
+    });
+
+  }
+
   render() {
+
+    const items = this.state.slotComponents.map((item)=><li>{item}</li>);
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
+          <h1>Slot Profile Creator</h1>
+          <p><img class="pikachu" src="https://i.kym-cdn.com/entries/icons/original/000/027/475/Screen_Shot_2018-10-25_at_11.02.15_AM.png"></img></p>
+          <h2>Slots</h2>
+
+          <ul>
+            {items}
+          </ul>
+
         </header>
       </div>
     );
+
   }
 }
 
---
+++ src/proxies/slotsAPI.js
@@ -0,0 +1,20 @@
+
+// proxy for a future Slots API
+
+class SlotsAPI {
+
+    constructor() {
+
+    }
+
+    all() {
+        return [
+            "EpicBanner", 
+            "BannerAd", 
+            "RelatedStories"
+        ];
+    }
+
+}
+
+export default SlotsAPI;
