2176d649e377656def8e4290d206f230f7207bf9
Matthew Walls <matthew.walls@guardian.co.uk>
2019-04-01 20:55:35 +0100

    Stub in material UI.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import SlotsAPI from './proxies/slotsAPI'
 
 class App extends Component {
@@ -31,15 +35,26 @@ class App extends Component {
 
     return (
       <div className="App">
+
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" color="inherit" noWrap>
+              Slot Machine
+            </Typography>
+          </Toolbar>
+        </AppBar>
         <header className="App-header">
           
-          <h1>Slot Profile Creator</h1>
           <p><img alt="pikachu with open mouth looking surprised" class="pikachu" src="https://i.kym-cdn.com/entries/icons/original/000/027/475/Screen_Shot_2018-10-25_at_11.02.15_AM.png"></img></p>
           <h2>Slots</h2>
 
           <ul>
             {items}
           </ul>
+
+          <Button variant="contained" color="primary">
+            New Profile
+          </Button>
 
         </header>
       </div>
