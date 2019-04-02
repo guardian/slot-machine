d60b08fe389cd3beeeb02c8bc7e3b60c3ab2b93c
Matthew Walls <matthew.walls@guardian.co.uk>
2019-04-02 14:56:04 +0100

    Stub in a rules screen

--- src/components/wizard.js
+++ src/components/wizard.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
-//import Button from '@material-ui/core/Button';
+
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+
 import { Exception } from 'handlebars';
 
 class SlotOutline extends Component {
@@ -47,6 +57,106 @@ class SlotChooserScreen extends Component {
             <SlotOutline widthToHeightRatio={1.5} name="onwards" onClick={()=>progress("onwards")}/>
 
             <h2>Choose a slot type!</h2>
+            
+        </div>
+        );
+  
+    }
+
+}
+
+class RulesScreen extends Component {
+
+    constructor() {
+
+        super();
+
+        this.state = {
+            contributor: true,
+            adblock: false,
+            somethingelse: false,
+          };
+
+    }
+
+    render() {
+
+        const { contributor, adblock, somethingelse } = this.state;
+
+        const handleChange = (a)=>{
+            if(a === "contributor"){
+                this.setState({contributor: !this.state.contributor})
+            } else if(a === "adblock"){
+                this.setState({adblock: !this.state.adblock})
+            } else if(a === "somethingelse"){
+                this.setState({somethingelse: !this.state.somethingelse})
+            }
+        };
+  
+        if(!this.props.onNext){
+            throw new Exception("No onNext defined for slot outline");
+        }
+        
+        return (
+        <div>
+
+            <form className="rules-form">
+
+                <div><FormControl margin="normal" fullWidth={true}>
+                    <InputLabel htmlFor="name-input">Name</InputLabel>
+                    <Input id="name-input" fullWidth={true} aria-describedby="name-helper-text" />
+                    <FormHelperText id="name-helper-text">A name for your slot profile</FormHelperText>
+                </FormControl></div>
+
+                <div><FormControl margin="normal" fullWidth={true}>
+                    <InputLabel htmlFor="abtest-input">AB Test</InputLabel>
+                    <Input id="abtest-input" fullWidth={true} aria-describedby="abtest-helper-text" />
+                    <FormHelperText id="abtest-helper-text">All slot profiles must have an AB Test</FormHelperText>
+                </FormControl></div>
+
+                <FormControl component="fieldset" margin="normal" fullWidth={true}>
+                    <FormLabel component="legend">Set Flags</FormLabel>
+                    <FormGroup>
+                        <FormControlLabel
+                        control={
+                            <Checkbox checked={contributor} onChange={()=>handleChange('contributor')} value="gilad" />
+                        }
+                        label="Must be contributor"
+                        />
+                        <FormControlLabel
+                        control={
+                            <Checkbox checked={adblock} onChange={()=>handleChange('adblock')} value="jason" />
+                        }
+                        label="Must have adblock enabled"
+                        />
+                        <FormControlLabel
+                        control={
+                            <Checkbox
+                            checked={somethingelse}
+                            onChange={()=>handleChange('somethingelse')}
+                            value="antoine"
+                            />
+                        }
+                        label="Something else"
+                        />
+                    </FormGroup>
+
+                    <div>
+                        <FormControl margin="normal" fullWidth={true}>
+                            <Button variant="contained" color="primary">
+                                Finish
+                            </Button>
+                        </FormControl>
+                    </div>
+
+
+
+                </FormControl>
+
+            </form>
+
+            <h2>Enter Rules!</h2>
+
             
         </div>
         );
@@ -121,7 +231,8 @@ class Wizard extends Component {
   
         const screens = [
             <SlotChooserScreen onNext={progress}/>,
-            <ComponentChooserScreen onNext={progress} />
+            <ComponentChooserScreen onNext={progress} />,
+            <RulesScreen onNext={progress} />
         ]
 
         return (
