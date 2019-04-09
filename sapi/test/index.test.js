038c577234f1b5c18e046efb251193e658cb82d4
Kate Whalen <whalenka88@gmail.com>
2019-04-09 12:14:57 +0100

    Add Jest to SAPI and give some example tests

---
+++ sapi/test/index.test.js
@@ -0,0 +1,13 @@
+
+describe('index tests', () => {
+    beforeEach(() => {
+    });
+    afterEach(() => {
+    });
+    it('should fail here', () => {
+        expect(true).toBe(false);
+    });
+    it('should pass here', () => {
+        expect(true).toBe(true);
+    });
+});
