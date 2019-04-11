b8c82fd5dd1c135ff7efcd8e5133212553c8bc22
Matthew Walls <matthew.walls@guardian.co.uk>
2019-04-11 10:22:55 +0100

    New slot names

--- src/proxies/slotsAPI.js
+++ src/proxies/slotsAPI.js
@@ -6,19 +6,19 @@ class SlotsAPI {
     constructor() {
 
         this._components = [
-            { name: "EpicBanner", slot: "banner", id: "EpicBanner" },
-            { name: "AskForMoneyBanner", slot: "banner", id:"AskForMoneyBanner" },
+            { name: "EpicBanner", slot: "overlay-banner-slot", id: "EpicBanner" },
+            { name: "AskForMoneyBanner", slot: "overlay-banner-slot", id:"AskForMoneyBanner" },
             { name: "RelatedStories", slot: "onwards", id:"RelatedStories" },
-            { name: "MostRead", slot: "onwards", id:"MostRead" },
-            { name: "LongestRead", slot: "onwards", id:"LongestRead" },
-            { name: "Outbrain", slot: "articleTopRight", id:"Outbrain" },
-            { name: "GoogleAd", slot: "articleTopRight", id:"GoogleAd" }
+            { name: "JoinTheGuardian", slot: "header-slot-a", id:"JoinTheGuardian" },
+            { name: "SomethingElse", slot: "header-slot-a", id:"SomethingElse" },
+            { name: "Outbrain", slot: "article-top-right", id:"Outbrain" },
+            { name: "GoogleAd", slot: "article-top-right", id:"GoogleAd" }
         ];
 
         this._slots = [
-            { name: "banner", height: 300, width: 900, widthToHeightRatio:3, id: "banner" },
-            { name: "articleTopRight", height: 300, width: 300, widthToHeightRatio:1, id: "articleTopRight" },
-            { name: "onwards", height: 300, width: 450, widthToHeightRatio:1.5, id: "onwards" }
+            { name: "overlay-banner-slot", height: 300, width: 900, widthToHeightRatio:3, id: "overlay-banner-slot" },
+            { name: "article-top-right", height: 300, width: 300, widthToHeightRatio:1, id: "article-top-right" },
+            { name: "header-slot-a", height: 300, width: 450, widthToHeightRatio:1.5, id: "header-slot-a" }
         ];
 
     }
